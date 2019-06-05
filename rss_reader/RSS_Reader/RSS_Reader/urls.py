bd94413571ac51592508f3165a0586cd453d4771
srikanth9493 <srikanth9949839402@gmail.com>
2019-06-05 23:42:03 +0530

    update Urls.py

--- rss_reader/RSS_Reader/RSS_Reader/urls.py
+++ rss_reader/RSS_Reader/RSS_Reader/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
+     path(r'', include(('reader.url', 'reader'), namespace='rss')),
     path('admin/', admin.site.urls),
-    path('rss/',include(('reader.url','reader'),namespace='rss'))
 
 ]

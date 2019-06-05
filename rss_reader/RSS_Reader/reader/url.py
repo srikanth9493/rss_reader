a83b1a017a7825846ab439093c8980dd0fe540f9
srikanth9493 <srikanth9949839402@gmail.com>
2019-06-05 23:41:02 +0530

    Update url.py

--- rss_reader/RSS_Reader/reader/url.py
+++ rss_reader/RSS_Reader/reader/url.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import Home_View,Feed_View
 
 urlpatterns = [
-    path('home/', Home_View.as_view()),
-    path('feeds/', Feed_View,name="feed"),
+      path('', Home_View.as_view()),
+      path('feeds/', Feed_View,name="feed"),
 ]
